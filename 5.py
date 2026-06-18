28bd3be7e5180fa57e81bd8180a8dfd33d5264f2
tanmayjethy8-a11y <tanmayjethy8@gmail.com>
2026-06-18 18:39:51 -0500

    rectified

--- 5.py
+++ 5.py
@@ -3,14 +3,14 @@ import numpy as np
 img = cv2.imread(r"C:\Users\HOME\OneDrive\Pictures\original_scenery image.jpeg")
 img = cv2.resize(img, (500, 300))
 b, g, r = cv2.split(img)
-cv2.imwrite("Bluechannel.jpg",b)
-cv2.imwrite("Greenchannel.jpg",g)
-cv2.imwrite("Redchannel.jpg",r)
-cv2.imshow("Blue Channel", b)
-cv2.imshow("Green Channel", g)
-cv2.imshow("Red Channel", r)
+zero=np.zeros_like(b)
+cv2.imshow("Blue Channel",cv2.merge([b,zero,zero]))
+cv2.imshow("Green Channel",cv2.merge([zero,g,zero]))
+cv2.imshow("Red Channel",cv2.merge([zero,zero,r]))
 
-
+cv2.imwrite("Blue_Channel.png",cv2.merge([b,zero,zero]))
+cv2.imwrite("Green_Channel.png",cv2.merge([zero,g,zero]))
+cv2.imwrite("Red_Channel.png",cv2.merge([zero,zero,r]))
 g = np.zeros_like(g)
 merged = cv2.merge([b, g, r])
 
